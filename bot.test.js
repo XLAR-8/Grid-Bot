2cc852c42938901e9e85600cc112a894c68d1120
XLAR-8 <ashoksingh201999@gmail.com>
2021-01-22 12:13:23 +0530

    fixes

--- bot.test.js
+++ bot.test.js
@@ -1,19 +1,22 @@
 const bot = require('./bot.js')
-bott = new bot(1,1);
+bott = new bot(1, 1);
 
 test('finds the path', () => {
     const steps = "FfLfR";
-    expect(bott.move(steps)).toEqual([3,2])
-    });
+    expect(bott.move(steps)).toEqual([3, 2])
+});
 
 test('Knows when bot is out of grid the', () => {
     const steps = "rFffrf";
     expect(bott.move(steps)).toContain('Botout');
-    });
+});
 
 test('knows where bot reached', () => {
     const position = "2,1";
     expect(bott.shortPath(position)).toContain('F');
-    });
+});
 
-
+test('check shortpath for exceeded size', () => {
+    const position = "20,20";
+    expect(bott.shortPath(position)).toContain('Size_exceded');
+});
